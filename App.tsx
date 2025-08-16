c8f86e008a08c61f16ef1ec437010ed8df9b6b9b
Christian-Tirado-CODE <tiradoc02@gmail.com>
2025-08-15 23:26:02 -0400

    CREDIT-KARMA: started 1st section in the Dashboard screen.

--- App.tsx
+++ App.tsx
@@ -5,12 +5,12 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import Dashboard from './src/Screens/Dashboard';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,10 +28,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+     <Dashboard safeAreaInsets={safeAreaInsets} />
     </View>
   );
 }
